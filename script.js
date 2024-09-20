bf4fe2a4364d82453a97f75ad440879751f85963
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-09-20 20:21:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function() {
-      const bands = [
+const bands = [
         'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
         'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
         'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive',
@@ -8,18 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
       ];
 
       // Function to remove "a", "an", "the" from the article titles
-      function strip(article) {
-        return article.replace(/^(a |an |the )/i, '').trim();
+      function strip(bandName) {
+        return bandName.replace(/^(a |an |the )/i, '').trim();
       }
 
       // Sort the articles by ignoring "a", "an", "the"
       const sortedBands = bands.sort((a, b) => strip(a).localeCompare(strip(b)));
 
       // Insert sorted articles into the ul#band
-      const ul = document.getElementById('band');
+      const bandList = document.getElementById('band');
       sortedBands.forEach(band => {
         const li = document.createElement('li');
         li.textContent = band;
-        ul.appendChild(li);
+        bandList.appendChild(li);
       });
     });
